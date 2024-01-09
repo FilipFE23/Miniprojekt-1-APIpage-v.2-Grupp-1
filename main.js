2cac69298d3cea262afa08e2a61c48348a8e00a2
stoffe-fe23 <kristoffer.bengtsson@gritacademy.se>
2024-01-09 13:20:24 +0100

    Handlers för sökform och navmeny

--- main.js
+++ main.js
@@ -3,36 +3,11 @@ import { getWeatherForecastByCity, getCurrentWeatherByCity, getCurrentPollutionB
 import { buildWeatherForecastsCallback, buildCurrentWeatherCallback, buildCurrentPollutionCallback, buildPollutionForecastCallback } from '../modules/interface.js';
 
 
-// ETT SNABBT FUL-TEST! - demo/test av getWeatherForecastByCity(), använd detta i event-handlers för sökformulär sen istället. 
-/*getWeatherForecastByCity(cityName) - Returnerar Promise med 5-dygnsprognos för väder i de städer vars namn matchar cityName
-
-    ANVÄNDNINGS-EXEMPEL:  (logga temperaturen vid första prognostillfället för 10 januari i första staden vars namn matchar "Stockholm")
-    -------------------
-    getWeatherForecastByCity("Stockholm").then((weatherSearchResult) => {
-        console.log(weatherSearchResult[0].forecasts["2024-01-10"][0].temperature);
-    });
-
-*/
-const cityName = "Malmö";
-document.querySelector("#homeresultsdiv").innerHTML = "";
-// Nuvarande väder
-getCurrentWeatherByCity(cityName, 1).then(buildCurrentWeatherCallback);
-// Femdygnsprognos
-getWeatherForecastByCity(cityName).then(buildWeatherForecastsCallback);
-
-document.querySelector("#pollutionresultsdiv").innerHTML = "";
-// Nuvarande föroreningar
-getCurrentPollutionByCity("Malmö").then(buildCurrentPollutionCallback);
-// Prognos för föroreningar
-getPollutionForecastByCity("Malmö").then(buildPollutionForecastCallback);
-
-
-/*
-// TODO: Submit handler för väder-sökning, behöver få FORM i html-filen först
-document.querySelector("#homesection form").addEventListener("submit", (event) => {
+// Submit handler för väder-sökning, behöver få FORM i html-filen först
+document.querySelector("#homeform").addEventListener("submit", (event) => {
     event.preventDefault();
-    const searchInput = document.querySelector("#homesearch");
-    
+    const searchInput = document.querySelector("#homesearch").value.trim();
+
     document.querySelector("#homeresultsdiv").innerHTML = "";
 
     // Nuvarande väder
@@ -40,15 +15,15 @@ document.querySelector("#homesection form").addEventListener("submit", (event) =
     // Femdygnsprognos
     getWeatherForecastByCity(searchInput).then(buildWeatherForecastsCallback);
 });
-*/
 
 
-/*
-// TODO: Submit handler för föroreningar-sökning, behöver få FORM i html-filen först
-document.querySelector("#pullutionsection form").addEventListener("submit", (event) => {
+
+
+// Submit handler för föroreningar-sökning, behöver få FORM i html-filen först
+document.querySelector("#pullutionform").addEventListener("submit", (event) => {
     event.preventDefault();
-    const searchInput = document.querySelector("#pullutionsearch");
-    
+    const searchInput = document.querySelector("#pullutionsearch").value.trim();
+
     document.querySelector("#pollutionresultsdiv").innerHTML = "";
 
     // Nuvarande föroreningar
@@ -56,10 +31,10 @@ document.querySelector("#pullutionsection form").addEventListener("submit", (eve
     // Prognos för föroreningar
     getPollutionForecastByCity(searchInput).then(buildPollutionForecastCallback);
 });
-*/
 
 
-// TODO: Navlänkar - visa rätt sektion
+
+// Navlänkar - visa rätt sektion
 document.querySelectorAll("body > header > nav > a").forEach((menuLink) => {
     menuLink.addEventListener("click", (event) => {
         event.preventDefault();
@@ -69,28 +44,28 @@ document.querySelectorAll("body > header > nav > a").forEach((menuLink) => {
         const aboutSection = document.querySelector("#aboutsection");
         const contactSection = document.querySelector("#contactsection");
 
-        if (event.currentTarget.id == "TODO: HOME-LÄNKENS ID HÄR") {
+        if (event.currentTarget.id == "headernavhome") {
             // Visa Home
             homeSection.classList.remove("hide");
             pollutionSection.classList.add("hide");
             aboutSection.classList.add("hide");
             contactSection.classList.add("hide");
         }
-        else if (event.currentTarget.id == "TODO: POLLUTION-LÄNKENS ID HÄR") {
+        else if (event.currentTarget.id == "headernavpollution") {
             // Visa air pollution
             homeSection.classList.add("hide");
             pollutionSection.classList.remove("hide");
             aboutSection.classList.add("hide");
             contactSection.classList.add("hide");
         }
-        else if (event.currentTarget.id == "TODO: ABOUT-LÄNKENS ID HÄR") {
+        else if (event.currentTarget.id == "headernavabout") {
             // Visa about
             homeSection.classList.add("hide");
             pollutionSection.classList.add("hide");
             aboutSection.classList.remove("hide");
             contactSection.classList.add("hide");
         }
-        else if (event.currentTarget.id == "TODO: CONTACT-LÄNKENS ID HÄR") {
+        else if (event.currentTarget.id == "headernavcontact") {
             // Visa contact
             homeSection.classList.add("hide");
             pollutionSection.classList.add("hide");
